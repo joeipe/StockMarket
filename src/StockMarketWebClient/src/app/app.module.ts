02ddbeefb9e5c7f7b95c34579f3d5e7cb98a49f4
Joe Ipe <Joe.Ipe@datacom.com.au>
2019-06-25 17:22:20 +1000

    Angular project setup

--- src/StockMarketWebClient/src/app/app.module.ts
+++ src/StockMarketWebClient/src/app/app.module.ts
@@ -3,6 +3,8 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ShellModule } from './shell/shell.module';
+import { HomeModule } from './home/home.module';
 
 @NgModule({
   declarations: [
@@ -10,7 +12,11 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+
+    ShellModule,
+    HomeModule,
+
+    AppRoutingModule // must be imported as the last module as it contains the fallback route
   ],
   providers: [],
   bootstrap: [AppComponent]
